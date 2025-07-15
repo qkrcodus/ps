c35b2a9af9b1f5df6859d78741a8266f2d0a369d
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2025-07-16 00:14:47 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 548 ms, Memory: 53060 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,43 +1,41 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException{
+        // scanner 대신 BufferedReader
+        // split 대신 StringTokenizer
+        final BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
-        // 공백 기준 파싱 dataNum, questionNum
-        String[] input1=br.readLine().split(" ");
-        int dataNum=Integer.parseInt(input1[0]);
-        int questionNum=Integer.parseInt(input1[1]);
+        // 공백 기준 파싱해서 dataNum, questionNum에 저장
+        final int dataNum=Integer.parseInt(st.nextToken());
+        final int questionNum=Integer.parseInt(st.nextToken());
 
-        // 숫자들 data[ ]에 저장
-        String[] input2=br.readLine().split(" ");
-        int[] data=new int[dataNum];
-        for (int i=0;i<dataNum;i++){
-            data[i]=Integer.parseInt(input2[i]);
-        }
-
-        // 데이터 수+1 만큼 sum 배열 0으로 초기화
-        int[] sum=new int[dataNum+1];
-        //Arrays.fill(sum,0); Java는 new int[] 시 자동으로 0 초기화되기에 불필요
-
-        // sum[i]=sum[i-1]+data[i]
+        // sum[ ] 생성 , 인덱스 일치를 위해 더미 0 추가
+        // StringTokenizer는 한 줄에 대한 파서 객체, BufferedReader는 계속 재사용 가능한 스트림
+        st=new StringTokenizer(br.readLine());
+        int sum[]=new int[dataNum+1];
         for (int i=1;i<=dataNum;i++){
-            sum[i]=sum[i-1]+data[i-1];
+            sum[i]=sum[i-1]+Integer.parseInt(st.nextToken());
         }
 
-        // questionNum만큼 반복 공백 기준 숫자 파싱해서 a, b 로 저장
-        for (int i=0;i<questionNum;i++) {
-            String[] input3 = br.readLine().split(" ");
-            int a = Integer.parseInt(input3[0]);
-            int b = Integer.parseInt(input3[1]);
-
-            bw.write(String.valueOf(sum[b] - sum[a - 1]));
-            bw.newLine();
+        // questionNum만큼 반복 공백 기준 숫자 파싱해서 startIndex, endIndex 로 저장
+        // bw.write(someValue)을 반복문 돌리면 출력 횟수가 많아진다. 출력은 StringBuilder
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<questionNum;i++){
+            st=new StringTokenizer(br.readLine());
+            final int startIndex=Integer.parseInt(st.nextToken());
+            final int endIndex=Integer.parseInt(st.nextToken());
+            sb.append(sum[endIndex]-sum[startIndex-1]).append("\n");
         }
 
-        br.close();
+        bw.write(sb.toString());
+        bw.flush();
         bw.close();
+        br.close();
+
     }
 }
+
