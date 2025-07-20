0f892c6aef133af96c08b856027b0bffba16a0e8
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2025-07-20 11:30:36 +0900

    [Gold IV] Title: 오큰수, Time: 980 ms, Memory: 147980 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // counts 받아오고, counts만큼 숫자 받아오기
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int counts=Integer.parseInt(br.readLine());
+
+        int[] numbers=new int[counts];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<counts;i++){
+            numbers[i]=Integer.parseInt(st.nextToken());
+        }
+
+        // 정답 배열 -1로 초기화
+        int[] result=new int[counts];
+        Arrays.fill(result,-1);
+
+        // 스택 쓰기
+        Stack<Integer> stack=new Stack<>();
+        stack.push(0);
+        for ( int i=0; i<counts;i++) {
+             while ( ! stack.isEmpty() && numbers[stack.peek()]<numbers[i]){
+                result[stack.pop()]=numbers[i];
+            }
+            stack.push(i);
+        }
+
+        // 출력하기
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<counts;i++){
+            sb.append(result[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
