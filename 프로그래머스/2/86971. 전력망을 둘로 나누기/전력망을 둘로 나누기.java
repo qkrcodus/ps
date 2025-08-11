3dccdb95b169c857ddd8a8fd45b895b7fd56c30d
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2025-08-11 14:50:33 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 13.58 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    private static List<Integer>[] graph;
+    private static boolean[] visited;
+    
+    public int solution(int n, int[][] wires) {
+        int answer = n-1;
+        // 양방향 그래프,visited[]
+        graph=new ArrayList[n+1];
+        for (int i=1;i<n+1;i++){
+            graph[i]=new ArrayList<>();
+        }
+        
+        for (int[] wire : wires){
+            graph[wire[0]].add(wire[1]);
+            graph[wire[1]].add(wire[0]);
+        }
+      
+        // 간선별로 제거된 상황에서 count 몇개인지 세기 count0, count1에 저장
+        // count0 초기화, dfs 단 돌고 있는 간선이웃 제외, 돌때마다 count0++
+        // count1 초기화, dfs 단 돌고 있는 간선이웃 제외, 돌때마다 count1++
+        // int diff = (count1>count0) ? count1-count0 : count0-count1
+        // answer<diff answer=diff;
+        
+        for (int[] wire : wires){
+            int count0,count1=0;
+            graph[wire[0]].remove(Integer.valueOf(wire[1]));
+            graph[wire[1]].remove(Integer.valueOf(wire[0]));
+            visited=new boolean[n+1];
+            count0=dfs(wire[0]);
+            count1=n-count0;
+            int diff = (count1>count0) ? count1-count0 : count0-count1;
+            if(answer>diff) answer=diff; 
+            
+            graph[wire[0]].add(wire[1]);
+            graph[wire[1]].add(wire[0]);
+        }        
+        return answer;
+    }
+   
+    private int dfs(int a){
+        visited[a]=true;
+        int count=1;
+        for ( int i : graph[a]){
+            if(!visited[i]){
+                count+=dfs(i);
+            }
+        }
+        return count;
+    }
+}
