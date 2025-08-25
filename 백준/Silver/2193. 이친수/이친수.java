371f10f61deb015ffccc02a68aacdc806e2937f5
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2025-08-25 14:28:39 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -3,10 +3,10 @@ import java.io.*;
 
 public class Main {
     private static long[] dp = new long[91];
-
+    private static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(br.readLine());
+        N=Integer.parseInt(br.readLine());
 
         init();
 
@@ -16,7 +16,7 @@ public class Main {
     private static void init() {
         dp[1] = 1;
         dp[2] = 1;
-        for (int i = 3; i <= 90; i++) {
+        for (int i = 3; i <= N; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
     }
