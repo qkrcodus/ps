44ffcb06ce85cfa7a582263a48aea7bb9a23720b
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2025-09-02 21:51:20 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String[] eachClothes : clothes){
+            map.put(eachClothes[1],map.getOrDefault(eachClothes[1],0)+1);
+        }
+        
+        int answer=1;
+        for(int value: map.values()){
+            answer*=(value+1);    
+        }
+       
+        return answer-1;
+    }
+}
